2b07f481ea2f5143ee3f630758c8c448d34389a8
Samuel A Parker <samuel.a.parker00@gmail.com>
2020-11-24 11:59:15 -0500

    updated search field title

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
         <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
             <div className="field">
-                <label>Image Search</label>
+                <label>Unsplash Image Search</label>
                 <input  type="text" 
                         value={this.state.term} 
                         onChange={e => this.setState({ term: e.target.value })} /> 
